20bab5ca173e6921a95389dd227ca73b95f60d12
Carmine DiMascio <dimascio@us.ibm.com>
2019-03-20 22:31:33 -0400

    provide complete example

--- README.md
+++ README.md
@@ -22,11 +22,77 @@ npm i express-middleware-openapi
 see [app.ts](test/app.ts) for an example.
 
 ```
-const app = express();
-
 new OpenApiMiddleware({
   apiSpecPath: './openapi.yaml',
   validateApiDoc: true, // default
   enableObjectCoercion: true, // should be default
 }).install(app);
+```
+
+## Example
+
+Try the complete example below:
+
+```
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+var logger = require('morgan');
+var http = require('http');
+var OpenApiMiddleware = require('express-middleware-openapi').OpenApiMiddleware;
+var app = express();
+
+app.use(bodyParser.json());
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+new OpenApiMiddleware({
+  apiSpecPath: './openapi.yaml',
+  validateApiDoc: true, // is the default
+  enableObjectCoercion: true, // should be default
+  errorTransformer: (a, b) => {
+    console.log('---error trans---', a, b);
+
+    return a;
+  },
+}).install(app);
+
+app.get('/v1/pets', function(req, res, next) {
+  console.log('at /v1/pets here');
+  res.json({
+    test: 'hi',
+  });
+});
+
+app.post('/v1/pets', function(req, res, next) {
+  res.json({
+    test: 'hi',
+  });
+});
+
+app.get('/v1/vets/:id', function(req, res, next) {
+  console.log('---- get /pets/:id', req.params);
+  // here
+  res.json({
+    id: req.params.id,
+  });
+});
+
+app.get('/v1/pets/:id', function(req, res, next) {
+  console.log('---- get /pets/:id', req.params);
+  // here
+  res.json({
+    id: req.params.id,
+  });
+});
+
+var server = http.createServer(app);
+server.listen(3000);
+console.log('Listening on port 3000');
+
+module.exports = app;
 ```
--- example/app.js
+++ example/app.js
@@ -4,8 +4,8 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var logger = require('morgan');
 var http = require('http');
-// var OpenApiMiddleware = require('./middleware').OpenApiMiddleware;
-var OpenApiMiddleware = require('express-middleware-openapi').OpenApiMiddleware
+var OpenApiMiddleware = require('./middleware').OpenApiMiddleware;
+// var OpenApiMiddleware = require('express-middleware-openapi').OpenApiMiddleware;
 var app = express();
 
 app.use(bodyParser.json());
@@ -15,19 +15,46 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use(new OpenApiMiddleware({
-    apiSpecPath: './openapi.yaml',
-    validate: true,
-    enableObjectCoercion: true // should be default
-}).middleware());
+new OpenApiMiddleware({
+  apiSpecPath: './openapi.yaml',
+  validateApiDoc: true, // is the default
+  enableObjectCoercion: true, // should be default
+  errorTransformer: (a, b) => {
+    console.log('---error trans---', a, b);
 
+    return a;
+  },
+}).install(app);
 
-app.get('/v1/pets', function (req, res, next) {
-    console.log('at /v1/pets here');
-    res.json({
-        test: 'hi'
-    });
+app.get('/v1/pets', function(req, res, next) {
+  console.log('at /v1/pets here');
+  res.json({
+    test: 'hi',
+  });
 });
+
+app.post('/v1/pets', function(req, res, next) {
+  res.json({
+    test: 'hi',
+  });
+});
+
+app.get('/v1/vets/:id', function(req, res, next) {
+  console.log('---- get /pets/:id', req.params);
+  // here
+  res.json({
+    id: req.params.id,
+  });
+});
+
+app.get('/v1/pets/:id', function(req, res, next) {
+  console.log('---- get /pets/:id', req.params);
+  // here
+  res.json({
+    id: req.params.id,
+  });
+});
+
 var server = http.createServer(app);
 server.listen(3000);
 console.log('Listening on port 3000');
