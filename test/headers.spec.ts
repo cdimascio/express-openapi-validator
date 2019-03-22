f7c1d866a06944333918bc5820904fcf21140bea
Carmine DiMascio <dimascio@us.ibm.com>
2019-03-22 15:24:09 -0400

    add test for header validation

---
+++ test/headers.spec.ts
@@ -0,0 +1,19 @@
+const expect = require('chai').expect;
+import * as request from 'supertest';
+import app from './app';
+const packageJson = require('../package.json');
+
+describe(packageJson.name, () => {
+  it('should throw 400 if required header is missing', async () =>
+    request(app)
+      .get('/v1/pets/10/attributes')
+      .set('Accept', 'application/json')
+      .expect('Content-Type', /json/)
+      .expect(400)
+      .then(r => {
+        const e = r.body.errors;
+        console.log(e)
+        expect(e).to.have.length(1);
+        expect(e[0].path).to.equal('x-attribute-id');
+      }));
+});
