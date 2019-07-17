f231a95b5d6ebcbbfc7a3b7837b994bf62f67100
Carmine DiMascio <cdimascio@tripadvisor.com>
2019-07-17 16:13:22 -0400

    initial integration of sheldhur req validator

--- src/middlewares/index.ts
+++ src/middlewares/index.ts
@@ -1,3 +1,4 @@
 export { applyOpenApiMetadata } from './openapi.metadata';
 export { validateRequest } from './openapi.request.validator';
+export { RequestValidator } from './openapi.request.validator.2';
 export { multipart } from './openapi.multipart';
