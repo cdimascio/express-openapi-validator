30f3472aaa830faaeca1c121e87c7e473ceec897
Carmine DiMascio <cdimascio@tripadvisor.com>
2019-07-21 13:58:47 -0400

    update example server

--- example/app.js
+++ example/app.js
@@ -7,6 +7,7 @@ var http = require('http');
 var OpenApiValidator = require('express-openapi-validator').OpenApiValidator;
 var app = express();
 
+app.use(bodyParser.urlencoded());
 app.use(bodyParser.json());
 app.use(logger('dev'));
 app.use(express.json());
@@ -14,42 +15,61 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+
+// 1. Install the OpenApiValidator on your express app
 new OpenApiValidator({
   apiSpecPath: '../openapi.yaml',
 }).install(app);
 
+// 2. Add routes
 app.get('/v1/pets', function(req, res, next) {
   res.json([{ id: 1, name: 'max' }, { id: 2, name: 'mini' }]);
 });
 
 app.post('/v1/pets', function(req, res, next) {
-  res.json({
-    name: 'sparky',
-  });
+  res.json({ name: 'sparky' });
 });
 
 app.get('/v1/pets/:id', function(req, res, next) {
+  res.json({ id: req.params.id, name: 'sparky' });
+});
+
+// 2a. Add a route upload file(s)
+app.post('/v1/pets/:id/photos', function(req, res, next) {
+  // DO something with the file
+  // files are found in req.files
+  // non file multipar params are in req.body['my-param']
+  console.log(req.files);
+
   res.json({
-    id: req.params.id,
-    name: 'sparky',
+    files_metadata: req.files.map(f => ({
+      originalname: f.originalname,
+      encoding: f.encoding,
+      mimetype: f.mimetype,
+      // Buffer of file conents
+      // buffer: f.buffer,
+    })),
   });
 });
 
-app.get('/v1/pets/:id/attributes', function(req, res, next) {
-  res.json({
-    id: req.params.id,
-    name: 'sparky',
-  });
-});
-
-// Register error handler!
+// 3. Create a custom error handler
 app.use((err, req, res, next) => {
   // format error
-  res.status(err.status).json({
-    errors: err.errors,
-  });
+  if (!err.status && !err.errors) {
+    res.status(500).json({
+      errors: [
+        {
+          message: err.message,
+        },
+      ],
+    });
+  } else {
+    res.status(err.status).json({
+      message: err.message,
+      errors: err.errors,
+    });
+  }
 });
-
 
 var server = http.createServer(app);
 server.listen(3000);
