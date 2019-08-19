00ab580b24bd7ed8d07fd59a1889043769993765
Andrey Trebler <at@edrilling.no>
2019-08-19 13:42:05 +0200

    adds tests

---
+++ test/path.level.parameters.spec.ts
@@ -0,0 +1,80 @@
+import * as path from 'path';
+import * as express from 'express';
+import { expect } from 'chai';
+import * as request from 'supertest';
+import { createApp } from './common/app';
+
+const packageJson = require('../package.json');
+
+describe(packageJson.name, () => {
+  let app = null;
+  let basePath = null;
+
+  before(async () => {
+    // Set up the express app
+    const apiSpec = path.join(
+      'test',
+      'resources',
+      'path.level.parameters.yaml',
+    );
+    app = await createApp({ apiSpec }, 3005);
+    basePath = app.basePath;
+
+    // Define new coercion routes
+    app.use(
+      `${basePath}`,
+      express
+        .Router()
+        .get(`/path_level_parameters`, (_req, res) => res.send())
+    );
+  });
+
+  after(() => {
+    app.server.close();
+  });
+
+  it('should return 400 if pathLevel query parameter is not provided', async () =>
+    request(app)
+      .get(`${basePath}/path_level_parameters?operationLevel=123`)
+      .send()
+      .expect(400)
+      .then(r => {
+        expect(r.body.errors).to.be.an('array')
+        expect(r.body.errors).to.have.length(1);
+        const message = r.body.errors[0].message;
+        expect(message).to.equal('should have required property \'pathLevel\'');
+      }));
+
+  it('should return 400 if operationLevel query parameter is not provided', async () =>
+    request(app)
+      .get(`${basePath}/path_level_parameters?pathLevel=123`)
+      .send()
+      .expect(400)
+      .then(r => {
+        expect(r.body.errors).to.be.an('array')
+        expect(r.body.errors).to.have.length(1);
+        const message = r.body.errors[0].message;
+        expect(message).to.equal('should have required property \'operationLevel\'');
+      }));
+
+  it('should return 400 if neither operationLevel, nor pathLevel query parameters are provided', async () =>
+    request(app)
+      .get(`${basePath}/path_level_parameters`)
+      .send()
+      .expect(400)
+      .then(r => {
+        expect(r.body.errors).to.be.an('array')
+        expect(r.body.errors).to.have.length(2);
+        const messages = r.body.errors.map(err => err.message);
+        expect(messages).to.have.members([
+          'should have required property \'pathLevel\'',
+          'should have required property \'operationLevel\''
+        ]);
+      }));
+
+  it('should return 200 if both pathLevel and operationLevel query parameter are provided', async () =>
+    request(app)
+      .get(`${basePath}/path_level_parameters?operationLevel=123&pathLevel=123`)
+      .send()
+      .expect(200));
+});
