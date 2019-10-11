e9e574d8dd0db2f5d5144500f45a088064759736
Carmine DiMascio <cdimascio@tripadvisor.com>
2019-10-11 18:50:26 -0400

    support apikey, bearer, and basic auth

--- test/security.spec.ts
+++ test/security.spec.ts
@@ -14,7 +14,6 @@ describe(packageJson.name, () => {
     apiSpec: path.join('test', 'resources', 'security.yaml'),
     securityHandlers: {
       ApiKeyAuth: function(req, scopes, schema) {
-        console.log('apikey handler throws custom error');
         throw { errors: [] };
       },
     },
@@ -28,7 +27,9 @@ describe(packageJson.name, () => {
       `${basePath}`,
       express
         .Router()
-        .get(`/api_key`, (req, res) => res.json({ logged_in: true })),
+        .get(`/api_key`, (req, res) => res.json({ logged_in: true }))
+        .get(`/bearer`, (req, res) => res.json({ logged_in: true }))
+        .get(`/basic`, (req, res) => res.json({ logged_in: true })),
     );
   });
 
@@ -50,7 +51,6 @@ describe(packageJson.name, () => {
 
   it('should return 401 if apikey handler returns false', async () => {
     eovConf.securityHandlers.ApiKeyAuth = <any>function(req, scopes, schema) {
-      console.log('apikey handler returns false');
       return false;
     };
     return request(app)
@@ -67,7 +67,6 @@ describe(packageJson.name, () => {
 
   it('should return 401 if apikey handler returns Promise with false', async () => {
     eovConf.securityHandlers.ApiKeyAuth = <any>function(req, scopes, schema) {
-      console.log('apikey handler returns promise false');
       return Promise.resolve(false);
     };
     return request(app)
@@ -84,7 +83,6 @@ describe(packageJson.name, () => {
 
   it('should return 401 if apikey header is missing', async () => {
     eovConf.securityHandlers.ApiKeyAuth = <any>function(req, scopes, schema) {
-      console.log('apikey handler returns promise false');
       return true;
     };
     return request(app)
@@ -105,39 +103,92 @@ describe(packageJson.name, () => {
     return request(app)
       .get(`${basePath}/api_key`)
       .set('X-API-Key', 'test')
-      .send({})
       .expect(200);
   });
 
-  // TODO add these tests
-  // it('should return 401 if auth header is missing for basic auth', async () => {
-  //   eovConf.securityHandlers.BasicAuth = <any>function(req, scopes, schema) {
-  //     return true;
-  //   };
-  //   return request(app)
-  //     .get(`${basePath}/api_key`)
-  //     .send({})
-  //     .expect(401);
-  // });
+  it('should return 401 if auth header is missing for basic auth', async () => {
+    (<any>eovConf.securityHandlers).BasicAuth = function(req, scopes, schema) {
+      return true;
+    };
+    return request(app)
+      .get(`${basePath}/basic`)
+      .expect(401)
+      .then(r => {
+        const body = r.body;
+        expect(body.errors).to.be.an('array');
+        expect(body.errors).to.have.length(1);
+        expect(body.errors[0].message).to.include('Authorization');
+      });
+  });
 
-  // it('should return 401 if auth header is missing for bearer auth', async () => {
-  //   eovConf.securityHandlers.BearerAuth = <any>function(req, scopes, schema) {
-  //     return true;
-  //   };
-  //   return request(app)
-  //     .get(`${basePath}/api_key`)
-  //     .send({})
-  //     .expect(401);
-  // });
+  it('should return 401 if auth header has malformed basic auth', async () => {
+    (<any>eovConf.securityHandlers).BasicAuth = <any>(
+      function(req, scopes, schema) {
+        return true;
+      }
+    );
+    return request(app)
+      .get(`${basePath}/basic`)
+      .set('Authorization', 'XXXX')
+      .expect(401)
+      .then(r => {
+        const body = r.body;
+        expect(body.errors).to.be.an('array');
+        expect(body.errors).to.have.length(1);
+        expect(body.errors[0].message).to.include(
+          "Authorization header with scheme 'Basic' required.",
+        );
+      });
+  });
 
-  // it('should return 401 if auth header is missing for malformed bearer auth', async () => {
-  //   eovConf.securityHandlers.BearerAuth = <any>function(req, scopes, schema) {
-  //     return true;
-  //   };
-  //   return request(app)
-  //     .get(`${basePath}/api_key`)
-  //     .set('Authorization', 'XXXX')
-  //     .send({})
-  //     .expect(401);
-  // });
+  it('should return 401 if auth header is missing for bearer auth', async () => {
+    (<any>eovConf.securityHandlers).BearerAuth = <any>(
+      function(req, scopes, schema) {
+        return true;
+      }
+    );
+    return request(app)
+      .get(`${basePath}/bearer`)
+      .expect(401)
+      .then(r => {
+        const body = r.body;
+        expect(body.errors).to.be.an('array');
+        expect(body.errors).to.have.length(1);
+        expect(body.errors[0].message).to.include('Authorization');
+      });
+  });
+
+  it('should return 401 if auth header has malformed bearer auth', async () => {
+    (<any>eovConf.securityHandlers).BearerAuth = <any>(
+      function(req, scopes, schema) {
+        return true;
+      }
+    );
+    return request(app)
+      .get(`${basePath}/bearer`)
+      .set('Authorization', 'XXXX')
+      .expect(401)
+      .then(r => {
+        const body = r.body;
+        expect(body.errors).to.be.an('array');
+        expect(body.errors).to.have.length(1);
+        expect(body.errors[0].message).to.include(
+          "Authorization header with scheme 'Bearer' required.",
+        );
+      });
+  });
+
+  it('should return 200 if bearer auth succeeds', async () => {
+    (<any>eovConf.securityHandlers).BearerAuth = <any>(
+      function(req, scopes, schema) {
+        return true;
+      }
+    );
+    return request(app)
+      .get(`${basePath}/bearer`)
+      .set('Authorization', 'Bearer XXXX')
+      .expect(200);
+  });
+
+  // TODO create tests for oauth2 and openid
 });
