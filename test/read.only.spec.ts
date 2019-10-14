a094b576333a2e92986fb508260fc59914f60bf5
Carmine DiMascio <cdimascio@tripadvisor.com>
2019-10-14 13:27:08 -0400

    create tests for read only support

---
+++ test/read.only.spec.ts
@@ -0,0 +1,58 @@
+import * as path from 'path';
+import * as express from 'express';
+import { expect } from 'chai';
+import * as request from 'supertest';
+import { createApp } from './common/app';
+
+const packageJson = require('../package.json');
+
+describe.only(packageJson.name, () => {
+  let app = null;
+
+  before(async () => {
+    // Set up the express app
+    const apiSpec = path.join('test', 'resources', 'read.only.yaml');
+    app = await createApp({ apiSpec, validateResponses: true }, 3005, app =>
+      app
+        .post(`${app.basePath}/products`, (req, res) => res.json(req.body))
+        .get(`${app.basePath}/products`, (req, res) =>
+          res.json([
+            {
+              id: 'id_1',
+              name: 'name_1',
+              price: 9.99,
+              created_at: new Date().toISOString(),
+            },
+          ]),
+        ),
+    );
+  });
+
+  after(() => {
+    app.server.close();
+  });
+
+  it('should not allow read only properties in requests', async () =>
+    request(app)
+      .get(`${app.basePath}/products`)
+      .query({
+        id: 'id_1',
+        name: 'some name',
+        price: 10.99,
+        created_at: new Date().toUTCString(),
+      })
+      .expect(400)
+      .then(r => {
+        const body = r.body;
+      }));
+
+  it('should allow read only properties in responses', async () =>
+    request(app)
+      .get(`${app.basePath}/products`)
+      .expect(200)
+      .then(r => {
+        expect(r.body)
+          .to.be.an('array')
+          .with.length(1);
+      }));
+});
