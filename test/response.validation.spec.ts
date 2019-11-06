c62a148892ba1e06e817818fb31f8e256f27a2b9
SpencerLawrenceBrown <spencerxbrown@gmail.com>
2019-11-05 20:33:15 -0500

    add null value support to response validation

--- test/response.validation.spec.ts
+++ test/response.validation.spec.ts
@@ -6,6 +6,7 @@ import { createApp } from './common/app';
 
 const packageJson = require('../package.json');
 const apiSpecPath = path.join('test', 'resources', 'response.validation.yaml');
+const today = new Date();
 
 describe(packageJson.name, () => {
   let app = null;
@@ -25,8 +26,13 @@ describe(packageJson.name, () => {
         });
         app.get(`${app.basePath}/pets`, (req, res) => {
           let json = {};
-          if ((req.query.mode = 'bad_type')) {
+          if ((req.query.mode == 'bad_type')) {
             json = [{ id: 'bad_id', name: 'name', tag: 'tag' }];
+          } else if (req.query.mode == 'check_null') {
+            json = [
+              { id: 1, name: 'name', tag: 'tag', bought_at: null },
+              { id: 2, name: 'name', tag: 'tag', bought_at: today.toISOString() },
+              { id: 3, name: 'name', tag: 'tag'}];
           }
           return res.json(json);
         });
@@ -97,6 +103,22 @@ describe(packageJson.name, () => {
         const e = r.body;
         expect(e.message).to.contain('should NOT have additional properties');
         expect(e.code).to.equal(500);
+      }));
+
+  it('should pass if value is null', async () =>
+    request(app)
+      .get(`${app.basePath}/pets?mode=check_null`)
+      .expect(200)
+      .then((r: any) => {
+        expect(r.body)
+          .is.an('array')
+          .with.length(3);
+        expect(r.body[0].bought_at)
+          .equal(null);
+        expect(r.body[1].bought_at)
+          .equal(today.toISOString());
+        expect(r.body[2].bought_at)
+          .to.be.undefined;
       }));
 
   it('should pass if response is a list', async () =>
