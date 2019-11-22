0b58c30c32cec492ebbac1fd5b4423988df6058b
Carmine DiMascio <cdimascio@tripadvisor.com>
2019-11-21 21:49:11 -0500

    Rejecting unknown query parameters should be optional #133

---
+++ test/query.params.allow.unknown.spec.ts
@@ -0,0 +1,54 @@
+import * as path from 'path';
+import * as express from 'express';
+import * as request from 'supertest';
+import { createApp } from './common/app';
+
+const packageJson = require('../package.json');
+
+describe(packageJson.name, () => {
+  let app = null;
+  let basePath = null;
+
+  before(async () => {
+    // Set up the express app
+    const apiSpec = path.join('test', 'resources', 'query.params.yaml');
+    app = await createApp(
+      { apiSpec, validateRequests: { allowUnknownQueryParameters: true } },
+      3005,
+      app =>
+        app.use(
+          `${app.basePath}`,
+          express
+            .Router()
+            .post(`/pets/nullable`, (req, res) => res.json(req.body)),
+        ),
+    );
+  });
+
+  after(() => {
+    app.server.close();
+  });
+
+  it('should pass if known query params are specified', async () =>
+    request(app)
+      .get(`${app.basePath}/pets`)
+      .query({
+        tags: 'one,two,three',
+        limit: 10,
+        breed: 'german_shepherd',
+        owner_name: 'carmine',
+      })
+      .expect(200));
+
+  it('should not fail if unknown query param is specified', async () =>
+    request(app)
+      .get(`${app.basePath}/pets`)
+      .query({
+        tags: 'one,two,three',
+        limit: 10,
+        breed: 'german_shepherd',
+        owner_name: 'carmine',
+        unknown_prop: 'test',
+      })
+      .expect(200));
+});
