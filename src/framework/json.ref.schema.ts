6daf769a940c9b1f1472d498a50060c1dc355f2b
Carmine DiMascio <cdimascio@tripadvisor.com>
2019-11-28 19:56:14 -0500

    async pass 1

--- src/framework/json.ref.schema.ts
+++ src/framework/json.ref.schema.ts
@@ -1,21 +1,2 @@
 import * as RefParser from 'json-schema-ref-parser';
-import { loopWhile } from 'deasync';
-
-export const $RefParser = {
-  bundle: (schema, options?) => {
-    var savedError,
-      savedResult,
-      done = false;
-
-    RefParser.bundle(schema, options, (error, result) => {
-      savedError = error;
-      savedResult = result;
-      done = true;
-    });
-
-    loopWhile(() => !done);
-
-    if (savedError) throw savedError;
-    return savedResult;
-  },
-};
+export const $RefParser = RefParser;
