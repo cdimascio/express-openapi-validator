0b87e66bab63249eb14518c9288ca30b7b110091
Jacob Ley <jacob.ley@showcasejobs.com>
2019-12-17 14:16:20 -0500

    Only enforce query object parsing when specified, always validate #186

--- test/serialized.objects.spec.ts
+++ test/serialized.objects.spec.ts
@@ -2,6 +2,7 @@ import * as path from 'path';
 import * as express from 'express';
 import * as request from 'supertest';
 import * as packageJson from '../package.json';
+import { expect } from 'chai';
 import { createApp } from './common/app';
 
 describe(packageJson.name, () => {
@@ -15,7 +16,7 @@ describe(packageJson.name, () => {
         `${app.basePath}`,
         express
           .Router()
-          .get(`/serialisable`, (req, res) => res.json({ id: 'test' })),
+          .get(`/serialisable`, (req, res) => res.json(req.query)),
       ),
     );
   });
@@ -29,6 +30,49 @@ describe(packageJson.name, () => {
       .get(`${app.basePath}/serialisable`)
       .query({
         settings: '{"onlyValidated":true}',
+        timestamp: '2019-06-24T12:34:56.789Z',
+        fooBar: '{"foo":"bar"}',
       })
-      .expect(200));
+      .expect(200)
+      .then(response => {
+        expect(response.body).to.deep.equal({
+          settings: {
+            onlyValidated: true,
+            onlySelected: [],
+          },
+          timestamp: '2019-06-24T12:34:56.789Z',
+          fooBar: {
+            foo: 'bar',
+          },
+        });
+      })
+  );
+
+  it('should not deserialize when non-object', async () =>
+    request(app)
+      .get(`${app.basePath}/serialisable`)
+      .query({
+        timestamp: 1234567890123,
+        fooBar: 'fooBar',
+      })
+      .expect(200)
+      .then(response => {
+        expect(response.body).to.deep.equal({
+          timestamp: 1234567890123,
+          fooBar: 'fooBar',
+        });
+      })
+  );
+
+  it('should fail on validation, not parsing', async () =>
+    request(app)
+      .get(`${app.basePath}/serialisable`)
+      .query({
+        settings: 'this is not valid json'
+      })
+      .expect(400)
+      .then(response => {
+        expect(response.body.message).to.equal('request.query.settings should be object');
+      })
+  );
 });
