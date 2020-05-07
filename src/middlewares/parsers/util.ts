ac9fd967b75cab9ad38f03be2915bc84bd3ced4f
Carmine DiMascio <cdimascio@tripadvisor.com>
2020-05-07 00:14:43 -0400

    deepObject default value support #287

--- src/middlewares/parsers/util.ts
+++ src/middlewares/parsers/util.ts
@@ -1,4 +1,7 @@
+import { Ajv } from 'ajv';
 import { OpenAPIV3 } from '../../framework/types';
+import ajv = require('ajv');
+import { OpenAPIFramework } from '../../framework';
 
 export function dereferenceParameter(
   apiDocs: OpenAPIV3.Document,
@@ -15,22 +18,62 @@ export function dereferenceParameter(
   }
 }
 
+// export function dereference(
+//   apiDocs: OpenAPIV3.Document,
+//   schema: OpenAPIV3.ReferenceObject | OpenAPIV3.SchemaObject,
+// ): OpenAPIV3.SchemaObject {
+//   // TODO this should recurse or use ajv.getSchema - if implemented as such, may want to cache the result
+//   // as it is called by query.paraer and req.parameter mutator
+
+//   if (schema?.['$ref']) {
+//     const ref = (<OpenAPIV3.ReferenceObject>schema).$ref;
+//     const id = ref.replace(/^.+\//i, '');
+//     if (apiDocs.components?.parameters?.[id]) {
+//       return <OpenAPIV3.SchemaObject>(
+//         (<unknown>apiDocs.components.parameters[id])
+//       );
+//     } else if (apiDocs.components?.schemas?.[id]) {
+//       return <OpenAPIV3.SchemaObject>apiDocs.components.schemas[id];
+//     }
+//   }
+//   return <OpenAPIV3.SchemaObject>schema;
+// }
+
 export function normalizeParameter(
+  ajv: Ajv,
   parameter: OpenAPIV3.ParameterObject,
 ): {
   name: string;
-  schema: OpenAPIV3.ReferenceObject | OpenAPIV3.SchemaObject;
+  schema: OpenAPIV3.SchemaObject;
 } {
-  // TODO this should recurse or use ajv.getSchema - if implemented as such, may want to cache the result
-  // as it is called by query.paraer and req.parameter mutator
-  let schema = parameter.schema;
+  let schema;
+  if (is$Ref(parameter)) {
+    schema = dereferenceSchema(ajv, parameter['$ref']);
+  } else if (parameter?.schema?.['$ref']) {
+    schema = dereferenceSchema(ajv, parameter.schema['$ref']);
+  } else {
+    schema = parameter.schema 
+  }
   if (!schema) {
     const contentType = Object.keys(parameter.content)[0];
     schema = parameter.content?.[contentType]?.schema;
   }
+  if (!schema) {
+    schema = parameter;
+  }
+
   const name =
     parameter.in === 'header' ? parameter.name.toLowerCase() : parameter.name;
   return { name, schema };
+}
+
+export function dereferenceSchema(ajv: Ajv, ref: string) {
+  // TODO cache schemas - so that we don't recurse every time
+  const derefSchema = ajv.getSchema(ref);
+  if (derefSchema?.['$ref']) {
+    return dereferenceSchema(ajv, '');
+  }
+  return derefSchema.schema;
 }
 
 function is$Ref(
