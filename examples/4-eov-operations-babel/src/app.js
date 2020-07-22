3a11f1965c36112115c3d0b55b3361b009d4489e
sydney <xtrycatchx@gmail.com>
2020-07-22 22:37:49 +0800

    removed unused dependency cookie-parser in examples and in readme, as well as exposing none existing public dir as static in express examples

--- examples/4-eov-operations-babel/src/app.js
+++ examples/4-eov-operations-babel/src/app.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const path = require('path');
-const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
 const logger = require('morgan');
 const http = require('http');
@@ -16,8 +15,6 @@ app.use(bodyParser.text());
 app.use(bodyParser.json());
 
 app.use(logger('dev'));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/spec', express.static(apiSpec));
 
