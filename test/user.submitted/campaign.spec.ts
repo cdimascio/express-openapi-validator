9ce0329ea33ebdeb47e3538e6433311939383cf1
Carmine DiMascio <cdimascio@gmail.com>
2020-08-27 18:29:00 -0400

    test for #356

---
+++ test/user.submitted/campaign.spec.ts
@@ -0,0 +1,46 @@
+import * as path from 'path';
+import * as express from 'express';
+import { expect } from 'chai';
+import * as request from 'supertest';
+import { createApp } from '../common/app';
+import * as packageJson from '../../package.json';
+
+describe(packageJson.name, () => {
+  let app = null;
+
+  before(async () => {
+    // Set up the express app
+    const apiSpec = path.join(__dirname, 'campaign.yaml');
+    app = await createApp({ apiSpec }, 3005, (app) =>
+      app.use(
+        express.Router().post(`/campaign`, (req, res) =>
+          res.status(201).json({
+            id: 123,
+            name: req.body.name,
+            description: req.body.description,
+            startDate: req.body.startDate,
+            createdAt: req.body.startDate,
+            updatedAt: req.body.updatedAt,
+          }),
+        ),
+      ),
+    );
+  });
+
+  after(() => {
+    app.server.close();
+  });
+
+  it('create campaign should return 201', async () => {
+    console.log(`campaign`);
+    return request(app)
+      .post(`/campaign`)
+      .send({
+        name: 'test',
+        description: 'description',
+        startDate: '2020-08-25T20:37:33.117Z',
+        endDate: '2020-08-25T20:37:33.117Z',
+      })
+      .expect(201);
+  });
+});
