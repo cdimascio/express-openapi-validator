e067d10cc1e31ee2daaa94285f6e4790b8c3b9cf
Carmine DiMascio <cdimasci@amazon.com>
2021-01-08 23:36:04 -0500

    fix: remove merge

--- src/framework/openapi.schema.validator.ts
+++ src/framework/openapi.schema.validator.ts
@@ -7,13 +7,7 @@ import { OpenAPIV3 } from './types.js';
 
 export class OpenAPISchemaValidator {
   private validator: Ajv.ValidateFunction;
-  constructor({
-    version,
-    extensions,
-  }: {
-    version: string;
-    extensions?: object;
-  }) {
+  constructor({ version }: { version: string; extensions?: object }) {
     const v = new Ajv({ schemaId: 'auto', allErrors: true });
     v.addMetaSchema(draftSchema);
 
@@ -21,9 +15,8 @@ export class OpenAPISchemaValidator {
     if (!ver) throw Error('version missing from OpenAPI specification');
     if (ver != 3) throw Error('OpenAPI v3 specification version is required');
 
-    const schema = merge({}, openapi3Schema, extensions ?? {});
-    v.addSchema(schema);
-    this.validator = v.compile(schema);
+    v.addSchema(openapi3Schema);
+    this.validator = v.compile(openapi3Schema);
   }
 
   public validate(
