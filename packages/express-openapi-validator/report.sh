cae54fc5e3c78704c71b5b2f1c2c8adf563e7420
Carmine DiMascio <cdimasci@amazon.com>
2021-01-18 14:35:23 -0500

    report script

--- packages/express-openapi-validator/report.sh
+++ packages/express-openapi-validator/report.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-bash <(curl -Ls https://coverage.codacy.com/get.sh) report --language typescript -r coverage/lcov.info
+bash <(curl -Ls https://coverage.codacy.com/get.sh) report --project-token "$CODACY_PROJECT_TOKEN" --language typescript -r coverage/lcov.info
