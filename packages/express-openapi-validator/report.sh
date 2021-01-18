dd6cba53f73e39bdd8fcf3d4132d3beb38527cdf
Carmine DiMascio <cdimasci@amazon.com>
2021-01-18 14:40:07 -0500

    fix indent

--- packages/express-openapi-validator/report.sh
+++ packages/express-openapi-validator/report.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
+echo "$CODACY_PROJECT_TOKEN"
 bash <(curl -Ls https://coverage.codacy.com/get.sh) report --project-token "$CODACY_PROJECT_TOKEN" --language typescript -r coverage/lcov.info
