a979e53e7755eb2e8a291b854d1c491415269926
Carmine DiMascio <cdimascio@gmail.com>
2021-11-27 18:41:58 -0500

    Update openapi.validator.ts

--- src/openapi.validator.ts
+++ src/openapi.validator.ts
@@ -321,7 +321,7 @@ export class OpenApiValidator {
   }
 
   private validateOptions(options: OpenApiValidatorOpts): void {
-    if (!options.apiSpec) throw ono('apiSpec required');
+    if (!options.apiSpec) throw ono('apiSpec required.');
 
     const securityHandlers = (<any>options).securityHandlers;
     if (securityHandlers != null) {
