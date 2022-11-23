b9e61445d04d054a1f577b876a11e32934292faa
Michael Eller <michael.eller@rakuten.com>
2022-11-23 23:48:05 +0000

    Remove examples from apiDoc when validating requests

--- src/middlewares/openapi.request.validator.ts
+++ src/middlewares/openapi.request.validator.ts
@@ -42,6 +42,8 @@ export class RequestValidator {
   ) {
     this.middlewareCache = {};
     this.apiDoc = apiDoc;
+    // Examples not needed for validation
+    delete this.apiDoc.components.examples;
     this.requestOpts.allowUnknownQueryParameters =
       options.allowUnknownQueryParameters;
     this.ajv = createRequestAjv(apiDoc, { ...options, coerceTypes: true });
