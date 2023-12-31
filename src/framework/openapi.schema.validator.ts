8f43042e4738b1f8236f55dd5dfa67b1f8cc6e0d
Sergio Ferreira <sergioferreira977@hotmail.com>
2023-12-31 09:57:37 +0000

    feat(openapi): support version 3.1

--- src/framework/openapi.schema.validator.ts
+++ src/framework/openapi.schema.validator.ts
@@ -6,7 +6,11 @@ import AjvDraft4, {
 import addFormats from 'ajv-formats';
 // https://github.com/OAI/OpenAPI-Specification/blob/master/schemas/v3.0/schema.json
 import * as openapi3Schema from './openapi.v3.schema.json';
+// https://github.com/OAI/OpenAPI-Specification/blob/master/schemas/v3.1/schema.json with dynamic refs replaced due to AJV bug - https://github.com/ajv-validator/ajv/issues/1745
+import * as openapi31Schema from './openapi.v3_1.modified.schema.json';
 import { OpenAPIV3 } from './types.js';
+
+import Ajv2020 from 'ajv/dist/2020';
 
 export interface OpenAPISchemaValidatorOpts {
   version: string;
@@ -17,7 +21,6 @@ export class OpenAPISchemaValidator {
   private validator: ValidateFunction;
   constructor(opts: OpenAPISchemaValidatorOpts) {
     const options: Options = {
-      schemaId: 'id',
       allErrors: true,
       validateFormats: true,
       coerceTypes: false,
@@ -29,15 +32,28 @@ export class OpenAPISchemaValidator {
       options.validateSchema = false;
     }
 
-    const v = new AjvDraft4(options);
-    addFormats(v, ['email', 'regex', 'uri', 'uri-reference']);
-
-    const ver = opts.version && parseInt(String(opts.version), 10);
+    const ver = opts.version && parseFloat(String(opts.version));
     if (!ver) throw Error('version missing from OpenAPI specification');
-    if (ver != 3) throw Error('OpenAPI v3 specification version is required');
+    if (parseInt(ver.toString()) != 3) throw Error('OpenAPI v3 specification version is required');
 
-    v.addSchema(openapi3Schema);
-    this.validator = v.compile(openapi3Schema);
+    let ajvInstance;
+    let schema;
+
+    if (ver === 3) {
+      schema = openapi3Schema;
+      ajvInstance = new AjvDraft4(options);
+    } else if (ver === 3.1) {
+      schema = openapi31Schema;
+      ajvInstance = new Ajv2020(options);
+      ajvInstance.addFormat('media-range', true); // TODO: Validate media-range format as defined in https://www.rfc-editor.org/rfc/rfc9110.html#name-collected-abnf
+    } else {
+      throw new Error('OpenAPI v3 specification 3.0 and 3.1 supported');
+    }
+
+    addFormats(ajvInstance, ['email', 'regex', 'uri', 'uri-reference']);
+
+    ajvInstance.addSchema(schema);
+    this.validator = ajvInstance.compile(schema);
   }
 
   public validate(openapiDoc: OpenAPIV3.Document): {
