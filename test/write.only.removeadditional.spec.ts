5db4dab019b98687f39fb8c7f576a0bf9fb595f4
cdimasci <cdimasci@amazon.com>
2024-02-09 21:44:27 -0500

    fixes write-only tests

--- test/write.only.removeadditional.spec.ts
+++ test/write.only.removeadditional.spec.ts
@@ -10,31 +10,34 @@ describe(packageJson.name, () => {
   before(async () => {
     // Set up the express app
     const apiSpec = path.join('test', 'resources', 'write.only.yaml');
-    app = await createApp({ apiSpec, validateResponses: {removeAdditional : true} }, 3005, app =>
-      app
-        .post(`${app.basePath}/products/inlined`, (req, res) => {
-          const body = req.body;
-          const excludeWriteOnly = req.query.exclude_write_only;
-          if (excludeWriteOnly) {
-            delete body.role;
-          }
-          res.json(body);
-        })
-        .post(`${app.basePath}/products/nested`, (req, res) => {
-          const body = req.body;
-          const excludeWriteOnly = req.query.exclude_write_only;
-          body.id = 'test';
-          body.created_at = new Date().toISOString();
-          body.reviews = body.reviews.map(r => ({
-            ...(excludeWriteOnly ? {} : { role_x: 'admin' }),
-            rating: r.rating ?? 2,
-          }));
+    app = await createApp(
+      { apiSpec, validateResponses: { removeAdditional: true } },
+      3005,
+      (app) =>
+        app
+          .post(`${app.basePath}/products/inlined`, (req, res) => {
+            const body = req.body;
+            const excludeWriteOnly = req.query.exclude_write_only;
+            if (excludeWriteOnly) {
+              delete body.role;
+            }
+            res.json(body);
+          })
+          .post(`${app.basePath}/products/nested`, (req, res) => {
+            const body = req.body;
+            const excludeWriteOnly = req.query.exclude_write_only;
+            body.id = 'test';
+            body.created_at = new Date().toISOString();
+            body.reviews = body.reviews.map((r) => ({
+              ...(excludeWriteOnly ? {} : { role_x: 'admin' }),
+              rating: r.rating ?? 2,
+            }));
 
-          if (excludeWriteOnly) {
-            delete body.role;
-          }
-          res.json(body);
-        }),
+            if (excludeWriteOnly) {
+              delete body.role;
+            }
+            res.json(body);
+          }),
     );
   });
 
@@ -52,7 +55,7 @@ describe(packageJson.name, () => {
         price: 10.99,
       })
       .expect(200)
-      .then(r => {
+      .then((r) => {
         const body = r.body;
         expect(body.message).to.be.undefined;
         expect(body.role).to.be.undefined;
@@ -71,8 +74,7 @@ describe(packageJson.name, () => {
         role: 'admin',
         price: 10.99,
       })
-      .expect(200)
-  );
+      .expect(200));
 
   it('should remove write only properties in responses (nested schema $refs) thanks to removeAdditional', async () =>
     request(app)
@@ -81,18 +83,18 @@ describe(packageJson.name, () => {
       .send({
         name: 'some name',
         price: 10.99,
+        password: 'password_value',
         reviews: [
           {
-            rating: 5
+            rating: 5,
+            review_password: 'review_password_value',
           },
         ],
       })
       .expect(200)
-      .then(r => {
+      .then((r) => {
         const body = r.body;
-        console.log('%j', r.body);
         expect(body.reviews[0].role_x).to.be.undefined;
         expect(body.reviews[0].rating).to.be.equal(5);
       }));
-
 });
