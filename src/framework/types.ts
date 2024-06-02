2f2afdfbe1dbbbbe057413aa5d01ffb5cc2bdee5
Duncan Beevers <duncan@dweebd.com>
2024-06-01 19:31:50 -0700

    chore: apiSpec may be const literal (#854)
    Co-authored-by: Carmine DiMascio <cdimascio@gmail.com>

--- src/framework/types.ts
+++ src/framework/types.ts
@@ -113,8 +113,26 @@ export type SerDesMap = {
   [format: string]: SerDes
 };
 
+type Primitive = undefined | null | boolean | string | number | Function
+
+type Immutable<T> =
+  T extends Primitive ? T :
+    T extends Array<infer U> ? ReadonlyArray<U> :
+      T extends Map<infer K, infer V> ? ReadonlyMap<K, V> : Readonly<T>
+
+type DeepImmutable<T> =
+  T extends Primitive ? T :
+    T extends Array<infer U> ? DeepImmutableArray<U> :
+      T extends Map<infer K, infer V> ? DeepImmutableMap<K, V> : DeepImmutableObject<T>
+
+interface DeepImmutableArray<T> extends ReadonlyArray<DeepImmutable<T>> {}
+interface DeepImmutableMap<K, V> extends ReadonlyMap<DeepImmutable<K>, DeepImmutable<V>> {}
+type DeepImmutableObject<T> = {
+  readonly [K in keyof T]: DeepImmutable<T[K]>
+}
+
 export interface OpenApiValidatorOpts {
-  apiSpec: OpenAPIV3.Document | string;
+  apiSpec: DeepImmutable<OpenAPIV3.Document> | string;
   validateApiSpec?: boolean;
   validateResponses?: boolean | ValidateResponseOpts;
   validateRequests?: boolean | ValidateRequestOpts;
