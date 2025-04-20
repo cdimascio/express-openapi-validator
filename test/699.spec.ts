b34e80a6a2b3848b2e58d32516803dbcfb82aea5
cdimascio <cdimascio@gmail.com>
2025-04-20 08:47:20 -0400

    fixes path syntax in 699 test

--- test/699.spec.ts
+++ test/699.spec.ts
@@ -54,7 +54,7 @@ describe('699', () => {
       },
       3005,
       (app) => {
-        app.get([`${app.basePath}/users/{:id}`], (req, res) => {
+        app.get([`${app.basePath}/users/:id`], (req, res) => {
           if (typeof req.params.id !== 'object') {
             throw new Error("Should be deserialized to ObjectId object");
           }
@@ -182,7 +182,7 @@ describe('699 serialize response components only', () => {
       },
       3005,
       (app) => {
-        app.get([`${app.basePath}/users/{:id}`], (req, res) => {
+        app.get([`${app.basePath}/users/:id`], (req, res) => {
           if (typeof req.params.id !== 'string') {
             throw new Error("Should be not be deserialized to ObjectId object");
           }
